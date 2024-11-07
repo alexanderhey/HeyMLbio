3503dbbbb123f92918dc5e1187ea474c86e4029c
Dan Reuman <reuman@ku.edu>
2024-11-07 14:13:11 -0600

    DAN: grade and comments for modularity

--- UnitModularity/usethefunction.R
+++ UnitModularity/usethefunction.R
@@ -1,3 +1,7 @@
+#DAN: Typically the function goes in another script and it is source'd and then run in main.R. 
+#You did the reverse.
+#DAN: The code does run on the first try, which is great.
+
 source("main.R")
 
 
@@ -6,3 +10,4 @@ analyze_climate_data("USAAnnualTemp1950_2008.rds", "data")
 
 #precipitation data
 analyze_climate_data("USAAnnualPcpn1950_2008.rds", "data")
+#DAN: Great that you have one function, run twice.
